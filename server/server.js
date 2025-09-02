276128a3888b3992e50d32061875dbd1831fb7d2
Jake Hirakawa <jake.hirakawa@griffithuni.edu.au>
2025-09-02 17:46:46 +1000

    added server and basic login connection

---
+++ server/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const lastId = 1;
+
+let users = [
+  { id: 1, username: "super", email: "super@example.com", roles: ["Super Admin"], groups: [] }
+];
+
+app.post("/api/auth/login", (req, res) => {
+    const {username, password} = req.body;
+    if (username === "super" && password === "123"){
+        return res.json({user: users[0]});
+    }
+    res.status(401).json({ error: "Invalid username/password" });
+})
+
+app.get("/api/users", (req, res) => {
+  res.json({ users });
+});
+
+app.post("/api/createuser", (req, res) => {
+  const { username, email } = req.body || {};
+  if (!username || !username.trim()) {
+    return res.status(400).json({ error: "username required" });
+  }
+  const exists = users.some(u => u.username.toLowerCase() === username.toLowerCase());
+  if (exists) {
+    return res.status(409).json({ error: "username taken" });
+  }
+  const user = {
+    id: lastId + 1,
+    username: username.trim(),
+    email: (email || "").trim(),
+    roles: ["User"],
+    groups: []          // will store group IDs
+  };
+  users.push(user);
+  res.status(201).json({ user });
+  lastId += 1;
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+    
